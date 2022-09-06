6e6041519099b376b7ae1178ad9dd759f0fb3555
anoop-k-n <anoopkn2001@pm.me>
2022-09-06 23:52:23 +0530

    Removed the previous three buttons. Instead, a bottom navigation view was created.

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setTheme(R.style.Theme_MusicPlayer)
         setContentView(R.layout.activity_main)
     }
 }
