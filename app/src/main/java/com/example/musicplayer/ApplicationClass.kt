8dd6ae1e14044e2e2ae61b38dd7c151342086179
anoop-k-n <anoopkn2001@pm.me>
2022-09-16 03:45:11 +0530

    Created an application class with the channels

--- app/src/main/java/com/example/musicplayer/ApplicationClass.kt
+++ app/src/main/java/com/example/musicplayer/ApplicationClass.kt
@@ -1,12 +1,26 @@
 package com.example.musicplayer
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.os.Build
 
 class ApplicationClass: Application() {
+    companion object{
+        const val CHANNEL_ID = "channel1"
+        const val PLAY = "play"  // use this for 'pause' as well
+        const val NEXT = "next"
+        const val PREVIOUS = "previous"
+        const val EXIT = "exit"
+    }
     override fun onCreate() {
         super.onCreate()
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(CHANNEL_ID, "Now Playing Song",
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.description = "This is an important channel for showing songs"
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
 
         }
     }
